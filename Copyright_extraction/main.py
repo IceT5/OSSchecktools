2f645b1c1d2b7bafb836772dc556c90dd8214efb
IceT5 <52195629+IceT5@users.noreply.github.com>
2026-01-28 18:15:24 +0800

    Correct copyright name from Icet5 to IceT5

--- Copyright_extraction/main.py
+++ Copyright_extraction/main.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2026 Icet5. All rights reserved
+# Copyright (c) 2026 IceT5. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
